c53dbd510fdc56d5d427f117c94cd3593f3ed7ab
Abhiram R <abhiramreghu09@gmail.com>
2018-01-28 17:09:58 +0530

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -1,4 +1,4 @@
-var menubtn = document.getElementsByClassName("circle-menubar-button")[0];
+	var menubtn = document.getElementsByClassName("circle-menubar-button")[0];
 	var btntxt = document.getElementById("btn-text");
 	var maincontainer = document.getElementsByClassName("main-container")[0];
 
