130b66458f1eb09d5c4b400c08f96e96cbfb4cf2
Abhiram R <abhiramreghu09@gmail.com>
2018-02-01 21:20:01 +0530

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -1,3 +1,4 @@
+
 	var menubtn = document.getElementsByClassName("circle-menubar-button")[0];
 	var btntxt = document.getElementById("btn-text");
 	var maincontainer = document.getElementsByClassName("main-container")[0];
@@ -5,9 +6,4 @@
 	menubtn.onclick = function() {
 			maincontainer.classList.toggle("show");
 			btntxt.classList.toggle("ion-close");
-			
-			for(var i=0;i<4;++i) {
-				var mainelement = document.getElementsByClassName("main-element")[i];
-				mainelement.classList.toggle("load");
-			}
 	}
